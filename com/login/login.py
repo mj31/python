76aea73c1c94a752924aef13e68b39c4f018689b
gaochangyu <906625611@>
2017-12-12 15:53:52 +0800

    提交

--- com/login/login.py
+++ com/login/login.py
@@ -1,32 +1,35 @@
-import requests
-import sys
 import io
+import sys
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
+import requests
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')  # 改变标准输出的默认编码
 
 #登录时需要POST的数据
-data = {'loginName':'mj',
-        'password':'123456',
-        'goto:http':'http://www.31build.com/bill/loginin.do',
-        'gotoOnFail:http':'http://www.31build.com/bill/login.do?fail=false'}
+data = {'loginName': 'mj',
+        'password': '123456',
+        'goto:http': 'http://www.31build.com/bill/loginin.do',
+        'gotoOnFail:http': 'http://www.31build.com/bill/login.do?fail=false'}
 
-#设置请求头
-headers = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+# 设置请求头
+headers = {
+    'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 '
+                  'Safari/537.36'}
 
-#登录时表单提交到的地址（用开发者工具可以看到）
+# 登录时表单提交到的地址（用开发者工具可以看到）
 login_url = 'http://www.31build.com/bill/loginin.do'
 
-#构造Session
+# 构造Session
 session = requests.Session()
 
-#在session中发送登录请求，此后这个session里就存储了cookie
-#可以用print(session.cookies.get_dict())查看
+# 在session中发送登录请求，此后这个session里就存储了cookie
+# 可以用print(session.cookies.get_dict())查看
 resp = session.post(login_url, data)
 
-#登录后才能访问的网页
+# 登录后才能访问的网页
 url = 'http://www.31build.com/bill/car/list.do'
 
-#发送访问请求
+# 发送访问请求
 resp = session.get(url)
 
 print(resp.content.decode('utf-8'))
---
+++ com/login/proxy_browser_login.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+import io
+from selenium import webdriver
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8') #改变标准输出的默认编码
+
+#建立Phantomjs浏览器对象，括号里是phantomjs.exe在你的电脑上的路径
+browser = webdriver.PhantomJS('D:/java/soft/phantomjs-2.1.1/bin/phantomjs.exe')
+
+#登录页面
+url = r'http://www.31build.com/bill/login.do'
+
+# 访问登录页面
+browser.get(url)
+
+# 等待一定时间，让js脚本加载完毕
+browser.implicitly_wait(3)
+
+#输入用户名
+username = browser.find_element_by_name('loginName')
+username.send_keys('mj')
+
+#输入密码
+password = browser.find_element_by_name('password')
+password.send_keys('123456')
+
+#选择“学生”单选按钮
+#student = browser.find_element_by_xpath('//input[@value="student"]')
+#student.click()
+
+
+browser.find_element_by_class_name('form-signin').submit()
+
+#网页截图
+browser.save_screenshot('picture1.png')
+#打印网页源代码
+print(browser.page_source.encode('utf-8').decode())
+
+browser.quit()
