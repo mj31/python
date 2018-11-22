238b59ffb7d8afce6b49bb78cd4d6c003a8663ad
gaochangyu <906625611@>
2018-11-22 19:12:58 +0800

    python基础练习

--- com/dangdang/dangdu.py
+++ com/dangdang/dangdu.py
@@ -2,12 +2,15 @@ import requests
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-    'Referer':'http://dangdu.dangdang.com/book/82607.shtml',
+    'Referer':'http://dangdu.dangdang.com',
     'Accept':'application/javascript, */*;q=0.8',
     'Host':'dangdu.dangdang.com',
 }
-payload = {"bookId":105813}
-rows = int(1000)
+payloadR = {"bookId":105821}
+payloadT = {"bookId":90138}
+rows = int(5000)
 for i in range(0, rows):
-    r = requests.get("http://dangdu.dangdang.com/book/click/ajaxBookClick.shtml",params=payload,headers = headers)
-    print(r.text)
+    r = requests.get("http://dangdu.dangdang.com/book/click/ajaxBookClick.shtml",params=payloadR,headers = headers)
+    print("r==="+r.text)
+    t = requests.get("http://dangdu.dangdang.com/book/click/ajaxBookClick.shtml",params=payloadT,headers = headers)
+    print("t==="+t.text)
