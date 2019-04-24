eabfea86a8fd467fecfd252386ca4c65f2709862
gaochangyu <906625611@>
2019-04-24 14:58:34 +0800

    python基础练习

--- com/dangdang/hf12345.py
+++ com/dangdang/hf12345.py
@@ -6,9 +6,9 @@ headers = {
     'Accept':'application/javascript, */*;q=0.8',
     'Host':'hf12345.vip',
 }
-payloadT = {"id":19031988080086,"isSearchPassWord":"22ca613f5555571f5e","tag":"91959a45556a02","sys_random":0.5853395519307}
+payloadT = {"id":19032988100058,"isSearchPassWord":"2c7c3628292157","tag":"91959a45556a02","sys_random":0.5853395519307}
 #2:onclick="detail('19031988080086','5f6b4f24065f041e4357','53174353e0777c7a08')
 #1:detail('19031988080086','5f6b4f24065f041e4357','53174353e0777c7a08');
-rows = int(800)
+rows = int(1000)
 for i in range(0, rows):
     r = requests.get("http://hf12345.vip/public/mhwz/todetail",params=payloadT,headers = headers)
